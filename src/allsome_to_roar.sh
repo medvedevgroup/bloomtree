edbb4fd35b320dbbcb52d8376e27330ad16607a6
Bob Harris <rsharris@bx.psu.edu>
2018-02-14 12:59:51 -0500

    added node count reporting

--- src/allsome_to_roar.sh
+++ src/allsome_to_roar.sh
@@ -4,20 +4,24 @@ uncompressedTree="$1"    # e.g. sbt.txt
 allsomeTree="$2"         # e.g. sbt-as.txt
 roarCompressedTree="$3"  # e.g. sbt-roar-allsome.txt
 
-# compress the SBT-AS bit vectors, creating the node files for a compressed
-# ABT-AS
+hashfile="example.hashfile"
 
+# compress the SBT-AS bit vectors, creating the node files for a compressed
+# SBT-AS
+
+nodeNum=0
 cat ${uncompressedTree} \
   | tr -d "*" \
   | sed "s/,.*//" \
   | sed "s/\.bf\.bv//" \
   | while read node ; do
-      echo "=== ROAR-compressing split ${node} ==="
-      bt compress-roar-single example.hashfile ${node}.bf-all.bv
-      bt compress-roar-single example.hashfile ${node}.bf-some.bv
+      nodeNum=$((nodeNum+1))
+      echo "=== #${nodeNum} ROAR-compressing split ${node} ==="
+      bt compress-roar-single ${hashfile} ${node}.bf-all.bv
+      bt compress-roar-single ${hashfile} ${node}.bf-some.bv
       done
 
-# create the topology file for the compressed ABT-AS.
+# create the topology file for the compressed SBT-AS.
 
 cat ${allsomeTree} \
   | sed "s/\.bv/.bv.roar/g" \
--- src/allsome_to_rrr.sh
+++ src/allsome_to_rrr.sh
@@ -4,22 +4,26 @@ uncompressedTree="$1"    # e.g. sbt.txt
 allsomeTree="$2"         # e.g. sbt-as.txt
 rrrCompressedTree="$3"   # e.g. sbt-rrr-allsome.txt
 
-# compress the SBT-AS bit vectors, creating the node files for a compressed
-# ABT-AS
+hashfile="example.hashfile"
 
+# compress the SBT-AS bit vectors, creating the node files for a compressed
+# SBT-AS
+
+nodeNum=0
 cat ${uncompressedTree} \
   | tr -d "*" \
   | sed "s/,.*//" \
   | sed "s/\.bf\.bv//" \
   | while read node ; do
-      echo "=== compressing split ${node} to allsome ==="
-      bt compress-rrr-double example.hashfile \
+      nodeNum=$((nodeNum+1))
+      echo "=== #${nodeNum} RRR-compressing split ${node} to single-file allsome ==="
+      bt compress-rrr-double ${hashfile} \
         ${node}.bf-all.bv \
         ${node}.bf-some.bv \
         ${node}.bf-allsome.bv
       done
 
-# create the topology file for the compressed ABT-AS.
+# create the topology file for the compressed SBT-AS.
 
 cat ${uncompressedTree} \
   | sed "s/\.bf\.bv/.bf-allsome.bv.rrr/" \
